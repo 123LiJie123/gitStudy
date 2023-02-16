3915a2176dfea957f0bfbc065a06abab360af0dd
123LiJie123 <1145485072@qq.com>
2023-02-16 12:38:06 +0800

    修改一下

--- src/main/java/com/example/gitxuexi/GitxuexiApplication.java
+++ src/main/java/com/example/gitxuexi/GitxuexiApplication.java
@@ -6,9 +6,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class GitxuexiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GitxuexiApplication.class, args);
-		System.out.println("傻逼");
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(GitxuexiApplication.class, args);
+        System.out.println("傻逼");
+
+        System.out.println("asdasdsad");
+    }
 
 }
